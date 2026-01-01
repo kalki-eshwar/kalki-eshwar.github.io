4cdd18fed7fd513700ff9062a43f21240b71ae74
Kalki Eshwar D <kalkieshward@gmail.com>
2026-01-01 15:02:21 +0530

    Attempt fix 4 GHW

--- src/utils/articles.ts
+++ src/utils/articles.ts
@@ -1,4 +1,5 @@
-import { serialize, MDXRemoteSerializeResult } from 'next-mdx-remote/serialize';
+import { serialize } from 'next-mdx-remote/serialize';
+import type { MDXRemoteSerializeResult } from 'next-mdx-remote';
 import remarkGfm from 'remark-gfm';
 import rehypeSlug from 'rehype-slug';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
